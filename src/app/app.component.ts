269e7a865091b81307b16dc187087795841c9452
muhammad-anas-ahmed-siddiqui <muhammadanas60606@gmail.com>
2022-03-17 16:25:27 +0500

    changing language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'transloco-example';
+
+  constructor(private translocoService: TranslocoService) {}
+
+  ngOnInit() {
+  }
+
+
+  changeLanguage(lang: string) {
+    this.translocoService.setActiveLang(lang)
+  }
+
 }
